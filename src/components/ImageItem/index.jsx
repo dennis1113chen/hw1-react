b8ad4006737f226c38b0b95daf87e6b2f3544e52
dennis1113chen <dennis1113.chen@gmail.com>
2023-03-11 19:42:06 +0800

    antd+CSS Grid+CSS Module

--- src/components/ImageItem/index.jsx
+++ src/components/ImageItem/index.jsx
@@ -1,36 +1,15 @@
+import styles from './imageItem.module.css'
+import '../../App.css';
 
-import styles from './productitem.module.css';
-
-export default function ProductItem({ product }) {
-
-   return (
-      <div className={styles.item}>
-         <a href="/">
-            <img
-               style={{ width: '100%' }}
-               src={product.image}
-               alt={product.name} />
-         </a>
-         <div className={styles.info}>
-            <h6 className={styles.category}>
-               {product.category}
-            </h6>
-            <h2 className={styles.name}>
-               {product.name}
-            </h2>
-            <p className={styles.description}>
-               {product.description}
-            </p>
-            <div className={styles.more}>
-               <a href="/" className={styles.link}>
-                  See More ...
-               </a>
-               <span
-                  className={styles.textGray}>
-                  USD {product.price}.00
-               </span>
+export default function ImageItem({ image}) { 
+    return (   
+            <div className={styles.image}>   
+                <a href="" className={styles.Link}>
+                    <img src={image.src}  className={styles.Style} />
+                </a>
+                <p className={styles.Title}> 
+                    {image.imageTitle}
+                </p>
             </div>
-         </div>
-      </div>
-   );
+    );
 }
