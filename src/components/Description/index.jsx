dc22d9c8c1831db7b111c9ebc9ee15413428ac73
dennis1113chen <dennis1113.chen@gmail.com>
2023-03-13 14:30:57 +0800

    antd+CSS Grid+CSS Module

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -8,7 +8,17 @@ export default function description({descriptiontitle,descriptioncontent}){
           <h1 className='h1'>{descriptiontitle}</h1>
           <hr className={styles.dividerLight} />
           <p className={styles.descriptionContent}>
-            {descriptioncontent}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum
+            corrupti ut iure aliquam. Quod, perferendis. Libero tempore iste, animi sint numquam illum quasi laboriosam
+            unde pariatur quidem! Suscipit consequatur nihil dolor impedit temporibus ad cum, voluptatum odit ratione
+            ullam
+            eligendi amet beatae aliquid eaque nesciunt optio nobis ducimus itaque. Voluptatibus aspernatur assumenda
+            quod
+            quo error consequuntur, vero iusto reprehenderit repellat maiores, sapiente dolores voluptates minima. Alias
+            dolore assumenda fuga quae repellat voluptatum quo, saepe exercitationem perspiciatis reiciendis modi
+            tempora.
+            Recusandae explicabo iusto corporis nam numquam dignissimos, necessitatibus, iure non culpa repellat
+            repudiandae temporibus! Saepe excepturi tempore iusto eos sit!
           </p>
         </div>
       </article>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,17 +17,7 @@ export default function Home(){
             />
             <DescriptionLayout
                 descriptiontitle="DESCRIPTIONS"
-                descriptioncontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum
-                corrupti ut iure aliquam. Quod, perferendis. Libero tempore iste, animi sint numquam illum quasi laboriosam
-                unde pariatur quidem! Suscipit consequatur nihil dolor impedit temporibus ad cum, voluptatum odit ratione
-                ullam
-                eligendi amet beatae aliquid eaque nesciunt optio nobis ducimus itaque. Voluptatibus aspernatur assumenda
-                quod
-                quo error consequuntur, vero iusto reprehenderit repellat maiores, sapiente dolores voluptates minima. Alias
-                dolore assumenda fuga quae repellat voluptatum quo, saepe exercitationem perspiciatis reiciendis modi
-                tempora.
-                Recusandae explicabo iusto corporis nam numquam dignissimos, necessitatibus, iure non culpa repellat
-                repudiandae temporibus! Saepe excepturi tempore iusto eos sit!"
+                
             />
             <Footer
                 address="臺北市和平東路二段134號，國立臺北教育大學 數位科技設計學系"
