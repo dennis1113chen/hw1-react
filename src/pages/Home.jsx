f1d09c9c9503f74715fe2d8aaf524d114c9a6cfb
dennis1113chen <dennis1113.chen@gmail.com>
2023-03-13 12:38:04 +0800

    antd+CSS Grid+CSS Module

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,6 @@ export default function Home(){
                 repudiandae temporibus! Saepe excepturi tempore iusto eos sit!"
             />
             <Footer
-
                 address="臺北市和平東路二段134號，國立臺北教育大學 數位科技設計學系"
                 copyright="&copy; Copyright All rights reserved."
             />
